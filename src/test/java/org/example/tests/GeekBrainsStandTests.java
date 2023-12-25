1756852ebd1590b852381d3c631ecc4a59a54ce1
Foxxlarr <talismafeed@yandex.ru>
2023-12-25 14:57:14 +0300

    1

---
+++ src/test/java/org/example/tests/GeekBrainsStandTests.java
@@ -0,0 +1,132 @@
+package org.example.tests;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
+import org.example.pom.LoginPage;
+import org.example.pom.MainPage;
+import org.example.pom.ProfilePage;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Пример использования самых базовых методов библиотеки Selenium.
+ */
+public class GeekBrainsStandTests {
+
+    private LoginPage loginPage;
+    private MainPage mainPage;
+
+    private static final String USERNAME = "Student-12";
+    private static final String PASSWORD = "31e146f2be";
+    private static final String FULL_NAME = "12 Student";
+
+
+    @BeforeEach
+    public void setupTest() {
+        // Навигация на https://test-stand.gb.ru/login с помощью Selenide
+        Selenide.open("https://test-stand.gb.ru/login");
+        // Сохраняем WebDriver из Selenide
+        WebDriver driver = WebDriverRunner.getWebDriver();
+        // Объект созданного Page Object
+        loginPage = new LoginPage(driver, new WebDriverWait(driver, Duration.ofSeconds(30)));
+    }
+
+    @Test
+    public void testLoginWithEmptyFields() {
+        // Клик на кнопку LOGIN без ввода данных в поля
+        loginPage.clickLoginButton();
+        // Проверка, что появился блок с ожидаемой ошибкой
+        assertEquals("401 Invalid credentials.", loginPage.getErrorBlockText());
+    }
+
+    @Test
+    public void testAddingGroupOnMainPage() {
+        // Логин в систему с помощью метода из класса Page Object
+        loginPage.login(USERNAME, PASSWORD);
+        // Инициализация объекта класса MainPage
+        mainPage = Selenide.page(MainPage.class);
+        assertTrue(mainPage.getUsernameLabelText().contains(USERNAME));
+        // Создание группы. Даём ей уникальное имя, чтобы в каждом запуске была проверка нового имени
+        String groupTestName = "New Test Group " + System.currentTimeMillis();
+        mainPage.createGroup(groupTestName);
+        // Проверка, что группа создана и находится в таблице
+        assertTrue(mainPage.waitAndGetGroupTitleByText(groupTestName).isDisplayed());
+    }
+
+    @Test
+    void testArchiveGroupOnMainPage() {
+        // Обычный логин + создание группы
+        loginPage.login(USERNAME, PASSWORD);
+        mainPage = Selenide.page(MainPage.class);
+        assertTrue(mainPage.getUsernameLabelText().contains(USERNAME));
+        String groupTestName = "New Test Group " + System.currentTimeMillis();
+        mainPage.createGroup(groupTestName);
+        // Требуется закрыть модальное окно
+        mainPage.closeCreateGroupModalWindow();
+        // Изменение созданной группы с проверками
+        assertEquals("active", mainPage.getStatusOfGroupWithTitle(groupTestName));
+        mainPage.clickTrashIconOnGroupWithTitle(groupTestName);
+        assertEquals("inactive", mainPage.getStatusOfGroupWithTitle(groupTestName));
+        mainPage.clickRestoreFromTrashIconOnGroupWithTitle(groupTestName);
+        assertEquals("active", mainPage.getStatusOfGroupWithTitle(groupTestName));
+    }
+
+    @Test
+    void testBlockingStudentInTableOnMainPage() {
+        // Обычный логин + создание группы
+        loginPage.login(USERNAME, PASSWORD);
+        mainPage = Selenide.page(MainPage.class);
+        assertTrue(mainPage.getUsernameLabelText().contains(USERNAME));
+        String groupTestName = "New Test Group " + System.currentTimeMillis();
+        mainPage.createGroup(groupTestName);
+        // Требуется закрыть модальное окно
+        mainPage.closeCreateGroupModalWindow();
+        // Добавление студентов
+        int studentsCount = 3;
+        mainPage.clickAddStudentsIconOnGroupWithTitle(groupTestName);
+        mainPage.typeAmountOfStudentsInCreateStudentsForm(studentsCount);
+        mainPage.clickSaveButtonOnCreateStudentsForm();
+        mainPage.closeCreateStudentsModalWindow();
+        mainPage.waitStudentsCount(groupTestName, studentsCount);
+        mainPage.clickZoomInIconOnGroupWithTitle(groupTestName);
+        // Проверка переходов статуса первого студента из таблицы
+        String firstGeneratedStudentName = mainPage.getStudentNameByIndex(0);
+        assertEquals("active", mainPage.getStatusOfStudentWithName(firstGeneratedStudentName));
+        mainPage.clickTrashIconOnStudentWithName(firstGeneratedStudentName);
+        assertEquals("block", mainPage.getStatusOfStudentWithName(firstGeneratedStudentName));
+        mainPage.clickRestoreFromTrashIconOnStudentWithName(firstGeneratedStudentName);
+        assertEquals("active", mainPage.getStatusOfStudentWithName(firstGeneratedStudentName));
+    }
+
+    @Test
+    void testFullNameOnProfilePage() {
+        // Логин в систему с помощью метода из класса Page Object
+        loginPage.login(USERNAME, PASSWORD);
+        // Инициализация объекта класса MainPage
+        mainPage = Selenide.page(MainPage.class);
+        assertTrue(mainPage.getUsernameLabelText().contains(USERNAME));
+        // Навигация на Profile page
+        mainPage.clickUsernameLabel();
+        mainPage.clickProfileLink();
+        // Инициализация ProfilePage с помощью Selenide
+        ProfilePage profilePage = Selenide.page(ProfilePage.class);
+        assertEquals(FULL_NAME, profilePage.getFullNameFromAdditionalInfo());
+        assertEquals(FULL_NAME, profilePage.getFullNameFromAvatarSection());
+    }
+
+    @AfterEach
+    public void teardown() {
+        // Закрываем все окна брайзера и процесс драйвера
+        WebDriverRunner.closeWebDriver();
+    }
+
+}
+
